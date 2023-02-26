2e0be1cca1330655298f153be228072e564e2a07
Oscar A. B. Orellana <oscar@softcorp.io>
2023-02-26 17:50:48 +0100

    updated

--- nuntio/projects/v1alpha1/projects_messages.pb.go
+++ nuntio/projects/v1alpha1/projects_messages.pb.go
@@ -70,6 +70,55 @@ func (APIKeyStatus) EnumDescriptor() ([]byte, []int) {
 	return file_nuntio_projects_v1alpha1_projects_messages_proto_rawDescGZIP(), []int{0}
 }
 
+type Mode int32
+
+const (
+	Mode_MODE_UNSPEFICIED  Mode = 0
+	Mode_MODE_MULTI_TENANT Mode = 1
+	Mode_MODE_MONO_TENANT  Mode = 2
+)
+
+// Enum value maps for Mode.
+var (
+	Mode_name = map[int32]string{
+		0: "MODE_UNSPEFICIED",
+		1: "MODE_MULTI_TENANT",
+		2: "MODE_MONO_TENANT",
+	}
+	Mode_value = map[string]int32{
+		"MODE_UNSPEFICIED":  0,
+		"MODE_MULTI_TENANT": 1,
+		"MODE_MONO_TENANT":  2,
+	}
+)
+
+func (x Mode) Enum() *Mode {
+	p := new(Mode)
+	*p = x
+	return p
+}
+
+func (x Mode) String() string {
+	return protoimpl.X.EnumStringOf(x.Descriptor(), protoreflect.EnumNumber(x))
+}
+
+func (Mode) Descriptor() protoreflect.EnumDescriptor {
+	return file_nuntio_projects_v1alpha1_projects_messages_proto_enumTypes[1].Descriptor()
+}
+
+func (Mode) Type() protoreflect.EnumType {
+	return &file_nuntio_projects_v1alpha1_projects_messages_proto_enumTypes[1]
+}
+
+func (x Mode) Number() protoreflect.EnumNumber {
+	return protoreflect.EnumNumber(x)
+}
+
+// Deprecated: Use Mode.Descriptor instead.
+func (Mode) EnumDescriptor() ([]byte, []int) {
+	return file_nuntio_projects_v1alpha1_projects_messages_proto_rawDescGZIP(), []int{1}
+}
+
 type File struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
@@ -903,14 +952,19 @@ var file_nuntio_projects_v1alpha1_projects_messages_proto_rawDesc = []byte{
 	0x12, 0x18, 0x0a, 0x14, 0x41, 0x50, 0x49, 0x4b, 0x45, 0x59, 0x5f, 0x53, 0x54, 0x41, 0x54, 0x55,
 	0x53, 0x5f, 0x41, 0x43, 0x54, 0x49, 0x56, 0x45, 0x10, 0x01, 0x12, 0x19, 0x0a, 0x15, 0x41, 0x50,
 	0x49, 0x4b, 0x45, 0x59, 0x5f, 0x53, 0x54, 0x41, 0x54, 0x55, 0x53, 0x5f, 0x52, 0x45, 0x56, 0x4f,
-	0x4b, 0x45, 0x44, 0x10, 0x02, 0x42, 0x6e, 0x0a, 0x18, 0x6e, 0x75, 0x6e, 0x74, 0x69, 0x6f, 0x2e,
-	0x70, 0x72, 0x6f, 0x6a, 0x65, 0x63, 0x74, 0x73, 0x2e, 0x76, 0x31, 0x61, 0x6c, 0x70, 0x68, 0x61,
-	0x31, 0x50, 0x01, 0x5a, 0x1a, 0x2e, 0x2f, 0x6e, 0x75, 0x6e, 0x74, 0x69, 0x6f, 0x2f, 0x70, 0x72,
-	0x6f, 0x6a, 0x65, 0x63, 0x74, 0x73, 0x2f, 0x76, 0x31, 0x61, 0x6c, 0x70, 0x68, 0x61, 0x31, 0xaa,
-	0x02, 0x18, 0x6e, 0x75, 0x6e, 0x74, 0x69, 0x6f, 0x2e, 0x70, 0x72, 0x6f, 0x6a, 0x65, 0x63, 0x74,
-	0x73, 0x2e, 0x76, 0x31, 0x61, 0x6c, 0x70, 0x68, 0x61, 0x31, 0xca, 0x02, 0x18, 0x4e, 0x75, 0x6e,
-	0x74, 0x69, 0x6f, 0x5c, 0x70, 0x72, 0x6f, 0x6a, 0x65, 0x63, 0x74, 0x73, 0x5c, 0x76, 0x31, 0x61,
-	0x6c, 0x70, 0x68, 0x61, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x4b, 0x45, 0x44, 0x10, 0x02, 0x2a, 0x49, 0x0a, 0x04, 0x4d, 0x6f, 0x64, 0x65, 0x12, 0x14, 0x0a,
+	0x10, 0x4d, 0x4f, 0x44, 0x45, 0x5f, 0x55, 0x4e, 0x53, 0x50, 0x45, 0x46, 0x49, 0x43, 0x49, 0x45,
+	0x44, 0x10, 0x00, 0x12, 0x15, 0x0a, 0x11, 0x4d, 0x4f, 0x44, 0x45, 0x5f, 0x4d, 0x55, 0x4c, 0x54,
+	0x49, 0x5f, 0x54, 0x45, 0x4e, 0x41, 0x4e, 0x54, 0x10, 0x01, 0x12, 0x14, 0x0a, 0x10, 0x4d, 0x4f,
+	0x44, 0x45, 0x5f, 0x4d, 0x4f, 0x4e, 0x4f, 0x5f, 0x54, 0x45, 0x4e, 0x41, 0x4e, 0x54, 0x10, 0x02,
+	0x42, 0x6e, 0x0a, 0x18, 0x6e, 0x75, 0x6e, 0x74, 0x69, 0x6f, 0x2e, 0x70, 0x72, 0x6f, 0x6a, 0x65,
+	0x63, 0x74, 0x73, 0x2e, 0x76, 0x31, 0x61, 0x6c, 0x70, 0x68, 0x61, 0x31, 0x50, 0x01, 0x5a, 0x1a,
+	0x2e, 0x2f, 0x6e, 0x75, 0x6e, 0x74, 0x69, 0x6f, 0x2f, 0x70, 0x72, 0x6f, 0x6a, 0x65, 0x63, 0x74,
+	0x73, 0x2f, 0x76, 0x31, 0x61, 0x6c, 0x70, 0x68, 0x61, 0x31, 0xaa, 0x02, 0x18, 0x6e, 0x75, 0x6e,
+	0x74, 0x69, 0x6f, 0x2e, 0x70, 0x72, 0x6f, 0x6a, 0x65, 0x63, 0x74, 0x73, 0x2e, 0x76, 0x31, 0x61,
+	0x6c, 0x70, 0x68, 0x61, 0x31, 0xca, 0x02, 0x18, 0x4e, 0x75, 0x6e, 0x74, 0x69, 0x6f, 0x5c, 0x70,
+	0x72, 0x6f, 0x6a, 0x65, 0x63, 0x74, 0x73, 0x5c, 0x76, 0x31, 0x61, 0x6c, 0x70, 0x68, 0x61, 0x31,
+	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -925,40 +979,41 @@ func file_nuntio_projects_v1alpha1_projects_messages_proto_rawDescGZIP() []byte 
 	return file_nuntio_projects_v1alpha1_projects_messages_proto_rawDescData
 }
 
-var file_nuntio_projects_v1alpha1_projects_messages_proto_enumTypes = make([]protoimpl.EnumInfo, 1)
+var file_nuntio_projects_v1alpha1_projects_messages_proto_enumTypes = make([]protoimpl.EnumInfo, 2)
 var file_nuntio_projects_v1alpha1_projects_messages_proto_msgTypes = make([]protoimpl.MessageInfo, 8)
 var file_nuntio_projects_v1alpha1_projects_messages_proto_goTypes = []interface{}{
 	(APIKeyStatus)(0),             // 0: nuntio.projects.v1alpha1.APIKeyStatus
-	(*File)(nil),                  // 1: nuntio.projects.v1alpha1.File
-	(*Folder)(nil),                // 2: nuntio.projects.v1alpha1.Folder
-	(*ApiAccess)(nil),             // 3: nuntio.projects.v1alpha1.ApiAccess
-	(*APIKey)(nil),                // 4: nuntio.projects.v1alpha1.APIKey
-	(*Image)(nil),                 // 5: nuntio.projects.v1alpha1.Image
-	(*AccessToken)(nil),           // 6: nuntio.projects.v1alpha1.AccessToken
-	(*Member)(nil),                // 7: nuntio.projects.v1alpha1.Member
-	(*Project)(nil),               // 8: nuntio.projects.v1alpha1.Project
-	(*timestamppb.Timestamp)(nil), // 9: google.protobuf.Timestamp
+	(Mode)(0),                     // 1: nuntio.projects.v1alpha1.Mode
+	(*File)(nil),                  // 2: nuntio.projects.v1alpha1.File
+	(*Folder)(nil),                // 3: nuntio.projects.v1alpha1.Folder
+	(*ApiAccess)(nil),             // 4: nuntio.projects.v1alpha1.ApiAccess
+	(*APIKey)(nil),                // 5: nuntio.projects.v1alpha1.APIKey
+	(*Image)(nil),                 // 6: nuntio.projects.v1alpha1.Image
+	(*AccessToken)(nil),           // 7: nuntio.projects.v1alpha1.AccessToken
+	(*Member)(nil),                // 8: nuntio.projects.v1alpha1.Member
+	(*Project)(nil),               // 9: nuntio.projects.v1alpha1.Project
+	(*timestamppb.Timestamp)(nil), // 10: google.protobuf.Timestamp
 }
 var file_nuntio_projects_v1alpha1_projects_messages_proto_depIdxs = []int32{
-	9,  // 0: nuntio.projects.v1alpha1.File.created_at:type_name -> google.protobuf.Timestamp
-	1,  // 1: nuntio.projects.v1alpha1.Folder.files:type_name -> nuntio.projects.v1alpha1.File
-	2,  // 2: nuntio.projects.v1alpha1.Folder.folders:type_name -> nuntio.projects.v1alpha1.Folder
-	9,  // 3: nuntio.projects.v1alpha1.APIKey.created_at:type_name -> google.protobuf.Timestamp
-	9,  // 4: nuntio.projects.v1alpha1.APIKey.updated_at:type_name -> google.protobuf.Timestamp
-	3,  // 5: nuntio.projects.v1alpha1.APIKey.mongo:type_name -> nuntio.projects.v1alpha1.ApiAccess
-	3,  // 6: nuntio.projects.v1alpha1.APIKey.postgres:type_name -> nuntio.projects.v1alpha1.ApiAccess
-	3,  // 7: nuntio.projects.v1alpha1.APIKey.storage:type_name -> nuntio.projects.v1alpha1.ApiAccess
-	3,  // 8: nuntio.projects.v1alpha1.APIKey.users:type_name -> nuntio.projects.v1alpha1.ApiAccess
+	10, // 0: nuntio.projects.v1alpha1.File.created_at:type_name -> google.protobuf.Timestamp
+	2,  // 1: nuntio.projects.v1alpha1.Folder.files:type_name -> nuntio.projects.v1alpha1.File
+	3,  // 2: nuntio.projects.v1alpha1.Folder.folders:type_name -> nuntio.projects.v1alpha1.Folder
+	10, // 3: nuntio.projects.v1alpha1.APIKey.created_at:type_name -> google.protobuf.Timestamp
+	10, // 4: nuntio.projects.v1alpha1.APIKey.updated_at:type_name -> google.protobuf.Timestamp
+	4,  // 5: nuntio.projects.v1alpha1.APIKey.mongo:type_name -> nuntio.projects.v1alpha1.ApiAccess
+	4,  // 6: nuntio.projects.v1alpha1.APIKey.postgres:type_name -> nuntio.projects.v1alpha1.ApiAccess
+	4,  // 7: nuntio.projects.v1alpha1.APIKey.storage:type_name -> nuntio.projects.v1alpha1.ApiAccess
+	4,  // 8: nuntio.projects.v1alpha1.APIKey.users:type_name -> nuntio.projects.v1alpha1.ApiAccess
 	0,  // 9: nuntio.projects.v1alpha1.APIKey.status:type_name -> nuntio.projects.v1alpha1.APIKeyStatus
-	9,  // 10: nuntio.projects.v1alpha1.APIKey.used_at:type_name -> google.protobuf.Timestamp
-	9,  // 11: nuntio.projects.v1alpha1.Image.expires_at:type_name -> google.protobuf.Timestamp
-	9,  // 12: nuntio.projects.v1alpha1.AccessToken.expires_at:type_name -> google.protobuf.Timestamp
-	9,  // 13: nuntio.projects.v1alpha1.AccessToken.created_at:type_name -> google.protobuf.Timestamp
-	9,  // 14: nuntio.projects.v1alpha1.Member.created_at:type_name -> google.protobuf.Timestamp
-	9,  // 15: nuntio.projects.v1alpha1.Member.updated_at:type_name -> google.protobuf.Timestamp
-	5,  // 16: nuntio.projects.v1alpha1.Project.logo:type_name -> nuntio.projects.v1alpha1.Image
-	9,  // 17: nuntio.projects.v1alpha1.Project.created_at:type_name -> google.protobuf.Timestamp
-	9,  // 18: nuntio.projects.v1alpha1.Project.updated_at:type_name -> google.protobuf.Timestamp
+	10, // 10: nuntio.projects.v1alpha1.APIKey.used_at:type_name -> google.protobuf.Timestamp
+	10, // 11: nuntio.projects.v1alpha1.Image.expires_at:type_name -> google.protobuf.Timestamp
+	10, // 12: nuntio.projects.v1alpha1.AccessToken.expires_at:type_name -> google.protobuf.Timestamp
+	10, // 13: nuntio.projects.v1alpha1.AccessToken.created_at:type_name -> google.protobuf.Timestamp
+	10, // 14: nuntio.projects.v1alpha1.Member.created_at:type_name -> google.protobuf.Timestamp
+	10, // 15: nuntio.projects.v1alpha1.Member.updated_at:type_name -> google.protobuf.Timestamp
+	6,  // 16: nuntio.projects.v1alpha1.Project.logo:type_name -> nuntio.projects.v1alpha1.Image
+	10, // 17: nuntio.projects.v1alpha1.Project.created_at:type_name -> google.protobuf.Timestamp
+	10, // 18: nuntio.projects.v1alpha1.Project.updated_at:type_name -> google.protobuf.Timestamp
 	19, // [19:19] is the sub-list for method output_type
 	19, // [19:19] is the sub-list for method input_type
 	19, // [19:19] is the sub-list for extension type_name
@@ -1074,7 +1129,7 @@ func file_nuntio_projects_v1alpha1_projects_messages_proto_init() {
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_nuntio_projects_v1alpha1_projects_messages_proto_rawDesc,
-			NumEnums:      1,
+			NumEnums:      2,
 			NumMessages:   8,
 			NumExtensions: 0,
 			NumServices:   0,
